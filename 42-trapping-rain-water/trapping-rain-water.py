f070276438c1d85c2eee162fa2bf68b47088077b
Raghu Ram Kotaru <123920680+raghuramkotaru@users.noreply.github.com>
2026-05-01 17:46:38 -0400

    Time: 14 ms (29.86%) | Memory: 20.9 MB (83.95%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -3,17 +3,19 @@ class Solution:
         n = len(h)
         pe = [0]*n
         po = [0]*n
-        pe[0] = h[0]
+        # pe[0] = h[0]
         res =0
+        ma =0
 
-        for i in range (1,n):
+        # for i in range (1,n):
 
-            pe[i] = max(h[i],pe[i-1])
+        #     pe[i] = max(h[i],pe[i-1])
         po[n-1] = h[n-1]
         for i in range(n-2,-1,-1):
             po[i] = max(h[i], po[i+1])
 
         for i in range(n):
-            res += min(po[i],pe[i])- h[i]
+            ma = max(ma,h[i])
+            res += min(ma,po[i])- h[i]
 
         return res
