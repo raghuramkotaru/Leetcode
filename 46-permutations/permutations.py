fef002a68058627d88c6c3d1defce0378bd52d34
Raghu Ram Kotaru <123920680+raghuramkotaru@users.noreply.github.com>
2026-05-11 21:41:12 -0400

    Time: 0 ms (100.00%) | Memory: 19.6 MB (34.58%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,18 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        def dfs(curr):
+            if len(curr)== len(nums):
+                res.append(curr.copy())
+                return
+            for num in nums:
+                if num not in curr:
+                    curr.append(num)
+                    dfs(curr)
+                    curr.pop()
+
+        dfs([])
+        return res
+
+
+            
