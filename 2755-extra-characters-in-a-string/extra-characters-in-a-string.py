c5a1b129a11743f2990e508b4cb054e76dfa879c
Raghu Ram Kotaru <123920680+raghuramkotaru@users.noreply.github.com>
2026-05-24 17:41:07 -0400

    Time: 75 ms (74.28%) | Memory: 25.3 MB (6.24%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.py
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.py
@@ -1,31 +1,36 @@
-# class Node:
-#     def __init__(self):
-#         self.ch = {}
-#         self.end = False
-# class Tree(words):
-#     self.root = Node()
-#     for i in words:
-#             cur = self.root
-#             for j in i:
-#                 if j not in cur.ch:
-#                     cur.ch[j] = Node()
-#                 cur = cur.ch[j]
-#         cur = self.root
+class Node:
+    def __init__(self):
+        self.ch = {}
+        self.end = False
+class Tree:
+    def __init__(self,words):
+        self.root = Node()
+        for i in words:
+            cur = self.root
+            for j in i:
+                if j not in cur.ch:
+                    cur.ch[j] = Node()
+                cur = cur.ch[j]
+            cur.end = True
 
 class Solution:
     def minExtraChar(self, s: str, dictionary: List[str]) -> int:
         
         words = set(dictionary)
         dp = {}
-        
+        tr = Tree(dictionary).root
         def dfs(i):
             if i == len(s):
                 return 0
             if i in dp:
                 return dp[i]
             res = 1+ dfs(i+1)
+            cur = tr
             for j in range(i,len(s)):
-                if s[i:j+1] in words:
+                if s[j] not in cur.ch:
+                    break
+                cur = cur.ch[s[j]]
+                if cur.end == True:
                     res = min(res, dfs(j+1))
             dp[i] = res
             return res
