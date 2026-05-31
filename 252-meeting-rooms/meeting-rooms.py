a210f735c27169e8e58bf6dbe355632f2adae4c7
Raghu Ram Kotaru <123920680+raghuramkotaru@users.noreply.github.com>
2026-05-30 22:00:10 -0400

    Time: 0 ms (100.00%) | Memory: 21.2 MB (78.67%) - LeetSync

--- 252-meeting-rooms/meeting-rooms.py
+++ 252-meeting-rooms/meeting-rooms.py
@@ -5,9 +5,9 @@ class Solution:
         intervals.sort(key=lambda i: i[0])
 
         for i in range(1, len(intervals)):
-            i1 = intervals[i - 1]
-            i2 = intervals[i]
+            i1 = intervals[i - 1][1]
+            i2 = intervals[i][0]
 
-            if i1[1] > i2[0]:
+            if i1>i2:
                 return False
         return True
