67fd33e25ae056d9293814b4d6dfd3c8994294d0
Raghu Ram Kotaru <123920680+raghuramkotaru@users.noreply.github.com>
2026-06-03 09:07:40 -0400

    Time: 0 ms (100.00%) | Memory: 19.5 MB (22.61%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -1,34 +1,34 @@
-# class Solution:
-
-#     
-#         nums.sort()
-#         res = []
-#         subset = []
-#         def dfs(i):
-#             res.append(subset.copy())
-#             for j in range(i, len(nums)):
-#                 if j > i and nums[j] == nums[j - 1]:
-#                     continue
-#                 subset.append(nums[j])
-#                 dfs(j + 1)
-#                 subset.pop()
-#         dfs(0)
-#         return res
 class Solution:
+
     def subsetsWithDup(self, nums):
+        nums.sort()
         res = []
         subset = []
-        nums.sort()
         def dfs(i):
-        
-            if i == len(nums):
-                if subset not in res:
-                    res.append(subset.copy())
-                return
-            subset.append(nums[i])
-            dfs(i+1)
-            subset.pop()
-            dfs(i+1)
+            res.append(subset.copy())
+            for j in range(i, len(nums)):
+                if j > i and nums[j] == nums[j - 1]:
+                    continue
+                subset.append(nums[j])
+                dfs(j + 1)
+                subset.pop()
         dfs(0)
-
         return res
+# class Solution:
+#     def subsetsWithDup(self, nums):
+#         res = []
+#         subset = []
+#         nums.sort()
+#         def dfs(i):
+        
+#             if i == len(nums):
+#                 if subset not in res:
+#                     res.append(subset.copy())
+#                 return
+#             subset.append(nums[i])
+#             dfs(i+1)
+#             subset.pop()
+#             dfs(i+1)
+#         dfs(0)
+
+#         return res
