2c04666487d94eb2c76ea5b87fb6b4d0d8ba907e
Raghu Ram Kotaru <123920680+raghuramkotaru@users.noreply.github.com>
2026-06-08 21:20:30 -0400

    Time: 7 ms (40.95%) | Memory: 19.9 MB (22.80%) - LeetSync

--- 47-permutations-ii/permutations-ii.py
+++ 47-permutations-ii/permutations-ii.py
@@ -1,23 +1,41 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        cur, res = [], []
-        ind = []
 
+        count = Counter(nums)
+        cur,res = [], []
         def bk(i):
             if i == len(nums):
                 res.append(cur.copy())
                 return
-            for j in range(len(nums)):
-                
-                if j in ind or (j > 0 and nums[j] == nums[j-1] and j-1 not in ind):
-                    
-                   continue
-                cur.append(nums[j])
-                ind.append(j)
-                bk(i+1)
-                cur.pop()
-                ind.remove(j)
-
+            for n in count:
+                if count[n] > 0:
+                    count[n] -= 1
+                    cur.append(n)
+                    bk(i+1)
+                    cur.pop()
+                    count[n] += 1
         bk(0)
         return res
+
+
+        # nums.sort()
+        # cur, res = [], []
+        # ind = []
+
+        # def bk(i):
+        #     if i == len(nums):
+        #         res.append(cur.copy())
+        #         return
+        #     for j in range(len(nums)):
+                
+        #         if j in ind or (j > 0 and nums[j] == nums[j-1] and j-1 not in ind):
+                    
+        #            continue
+        #         cur.append(nums[j])
+        #         ind.append(j)
+        #         bk(i+1)
+        #         cur.pop()
+        #         ind.remove(j)
+
+        # bk(0)
+        # return res
