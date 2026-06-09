bc430e8474d43d1b43e0085a5b4b6a54de0e1b3f
Raghu Ram Kotaru <123920680+raghuramkotaru@users.noreply.github.com>
2026-06-09 00:03:07 -0400

    Time: 193 ms (64.58%) | Memory: 19.3 MB (86.81%) - LeetSync

--- 473-matchsticks-to-square/matchsticks-to-square.py
+++ 473-matchsticks-to-square/matchsticks-to-square.py
@@ -1,25 +1,51 @@
+# class Solution:
+#     def makesquare(self, m: List[int]) -> bool:
+#         m.sort(reverse = True)
+#         l = sum(m)//4
+#         if sum(m) %4 != 0:
+#             return False
+#         if max(m) > l:
+#             return False
+#         side = [0] * 4
+#         def bk(i):
+#             if i == len(m):
+#                 return True
+
+#             for j in range(4):
+#                 if side[j]+ m[i] <= l:
+#                     side[j] += m[i]
+#                     if bk(i+1):
+#                         return True
+#                     side[j] -= m[i]
+#             return False
+#         return bk(0)
+        
 class Solution:
-    def makesquare(self, m: List[int]) -> bool:
-        m.sort(reverse = True)
-        l = sum(m)//4
-        if sum(m) %4 != 0:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        total_length = sum(matchsticks)
+        if total_length % 4 != 0:
             return False
-        if max(m) > l:
-            return False
-        side = [0] * 4
-        def bk(i):
-            if i == len(m):
+
+        length = total_length // 4
+        sides = [0] * 4
+        matchsticks.sort(reverse=True)
+
+        def dfs(i):
+            if i == len(matchsticks):
                 return True
 
-            for j in range(4):
-                if side[j]+ m[i] <= l:
-                    side[j] += m[i]
-                    if bk(i+1):
+            for side in range(4):
+                if sides[side] + matchsticks[i] <= length:
+                    sides[side] += matchsticks[i]
+                    if dfs(i + 1):
                         return True
-                    side[j] -= m[i]
-            return False
-        return bk(0)
-        
+                    sides[side] -= matchsticks[i]
 
+                if sides[side] == 0:
+                    break
+
+            return False
+
+        return dfs(0)
         
 
