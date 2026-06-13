91f46eeba962187c6d31c0a8fa9bc271323a9194
Raghu Ram Kotaru <123920680+raghuramkotaru@users.noreply.github.com>
2026-06-13 15:36:09 -0400

    Time: 240 ms (69.13%) | Memory: 21.6 MB (48.91%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -11,19 +11,20 @@ class Solution:
             grid[r][c] = "0"
             q = deque()
             q.append((r,c))
+            a = [[-1,0],[1,0],[0,-1],[0,1]]
             while q:
                 ro,co = q.popleft()
-                a = [[-1,0],[1,0],[0,-1],[0,1]]
+                
                 for dr,dc in a:
                     r,c = dr+ro, dc+co
-                    if 0 <= r < rows and 0 <= c < cols and grid[r][c] == "1" and (r,c):
+                    if 0 <= r < rows and 0 <= c < cols and grid[r][c] == "1":
                         q.append((r,c))
                         grid[r][c] = "0"
                         # visit.add((r,c))
 
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == "1" and (r,c):
+                if grid[r][c] == "1":
                     
                     dfs(r,c)
                     count += 1
