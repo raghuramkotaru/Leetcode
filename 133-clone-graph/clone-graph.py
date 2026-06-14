f938742f1656568c47408301ee8c158d2d176e6c
Raghu Ram Kotaru <123920680+raghuramkotaru@users.noreply.github.com>
2026-06-14 15:13:55 -0400

    Time: 50 ms (54.76%) | Memory: 19.7 MB (55.01%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -6,33 +6,20 @@ class Node:
         self.neighbors = neighbors if neighbors is not None else []
 """
 
-
+from typing import Optional
 class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node: return None
+        hashmap = {}
 
-    def __init__(self):
-        # Dictionary to save the visited node and it's respective clone
-        # as key and value respectively. This helps to avoid cycles.
-        self.visited = {}
+        def dfs(node):
+            if node in hashmap:
+                return hashmap[node]
 
-    def cloneGraph(self, node: Optional["Node"]) -> Optional["Node"]:
-        if not node:
-            return node
-
-        # If the node was already visited before.
-        # Return the clone from the visited dictionary.
-        if node in self.visited:
-            return self.visited[node]
-
-        # Create a clone for the given node.
-        # Note that we don't have cloned neighbors as of now, hence [].
-        clone_node = Node(node.val, [])
-
-        # The key is original node and value being the clone node.
-        self.visited[node] = clone_node
-
-        # Iterate through the neighbors to generate their clones
-        # and prepare a list of cloned neighbors to be added to the cloned node.
-        if node.neighbors:
-            clone_node.neighbors = [self.cloneGraph(n) for n in node.neighbors]
-
-        return clone_node
+            copy = Node(node.val)
+            hashmap[node] = copy
+            for n in node.neighbors:
+                copy.neighbors.append(dfs(n))
+            return copy
+        
+        return dfs(node)
