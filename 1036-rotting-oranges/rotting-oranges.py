7c5f800494f3492425700141582d3deb2fa870f2
Raghu Ram Kotaru <123920680+raghuramkotaru@users.noreply.github.com>
2026-06-15 00:44:36 -0400

    Time: 3 ms (76.42%) | Memory: 19.2 MB (78.99%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,23 +1,27 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         q = deque()
-        
+        visit =set()
+        fresh = 0
         rows,cols = len(grid),len(grid[0])
         for r in range (rows):
             for c in range (cols):
                 if grid[r][c] == 2:
                     q.append((r,c))
-                    
+                    visit.add((r,c))
+                if grid[r][c]== 1:
+                    fresh+= 1
         def bfs(r,c):
-            
-            if 0<=r<rows and 0<= c<cols and grid[r][c] == 1:
-                
+            nonlocal fresh
+            if 0<=r<rows and 0<= c<cols and grid[r][c] == 1 and (r,c) not in visit:
+                visit.add((r,c))
                 q.append((r,c))
-                grid[r][c] = 2
+                grid[r][c] == 2
+                fresh -= 1
             return
 
         time = 0
-        while q:
+        while q and fresh > 0:
             
             for i in range(len(q)):
                 r,c = q.popleft()
@@ -25,13 +29,11 @@ class Solution:
                 bfs(r,c-1)
                 bfs(r+1,c)
                 bfs(r-1,c)
-            if q:
-                time += 1
-        for r in range (rows):
-            for c in range (cols):
-                if grid[r][c] == 1:
-                    return -1
-        return time
+            time += 1
+        
+        return time if fresh == 0 else -1
                 
               
+
+
 
