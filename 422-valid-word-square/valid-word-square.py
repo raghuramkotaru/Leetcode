4b096ab59d0512af09825cd9c956b6706c22d3fa
Raghu Ram Kotaru <123920680+raghuramkotaru@users.noreply.github.com>
2026-06-16 00:41:15 -0400

    Time: 19 ms (66.67%) | Memory: 19.9 MB (33.33%) - LeetSync

--- 422-valid-word-square/valid-word-square.py
+++ 422-valid-word-square/valid-word-square.py
@@ -1,8 +1,9 @@
 class Solution:
     def validWordSquare(self, words: List[str]) -> bool:
         for i in range(len(words)):
-            vals = []
-            for ii in range(len(words)):
-                if i < len(words[ii]): vals.append(words[ii][i])
-            if words[i] != "".join(vals): return False 
-        return True 
+            
+            for j in range(len(words[i])):
+                if j >= len(words) or i >= len(words[j]) or words[i][j] != words[j][i]:
+                    return False
+
+        return True
