c38739dbed2c004b20524ea29ca108665165740d
Raghu Ram Kotaru <123920680+raghuramkotaru@users.noreply.github.com>
2026-06-17 01:37:34 -0400

    Time: 0 ms (100.00%) | Memory: 21.3 MB (26.30%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -8,8 +8,8 @@ class Solution:
         def dfs(course):
             if course in visit:
                 return False
-            if h[course] == []:
-                return True
+            # if h[course] == []:
+            #     return True
             visit.add(course)
             for pre in h[course]:
                 if not dfs(pre):
@@ -22,3 +22,34 @@ class Solution:
                 return False
         return True
                 
+
+
+
+
+
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # Map each course to its prerequisites
+        preMap = {i: [] for i in range(numCourses)}
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+        visiting = set()
+        def dfs(crs):
+            if crs in visiting:
+                return False
+            if preMap[crs] == []:
+                return True
+
+            visiting.add(crs)
+            for pre in preMap[crs]:
+                if not dfs(pre):
+                    return False
+            visiting.remove(crs)
+            preMap[crs] = []
+            return True
+
+        for c in range(numCourses):
+            if not dfs(c):
+                return False
+        return True
