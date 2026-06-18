7568927919c359279a0bad67766de8339a97f4f0
Raghu Ram Kotaru <123920680+raghuramkotaru@users.noreply.github.com>
2026-06-18 19:44:18 -0400

    Time: 0 ms (100.00%) | Memory: 21.5 MB (7.33%) - LeetSync

---
+++ 261-graph-valid-tree/graph-valid-tree.py
@@ -0,0 +1,26 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        h = {i:[] for i in range(n)}
+
+        for i,j in edges:
+            h[i].append(j)
+            h[j].append(i)
+
+        visit = set()
+        def dfs(node,parent):
+            if node in visit:
+                return False
+            visit.add(node)
+            for i in h[node]:
+                if i == parent:
+                    continue
+                if dfs(i,node) == False:
+                    return False
+            return True
+        if dfs(0,-1):
+            for i in range(n):
+                if i not in visit:
+                    return False    
+            return True
+        return False
+
