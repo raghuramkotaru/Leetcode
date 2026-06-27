51b5ed5ad197c474e7b5d553175063a0da33e1bd
Raghu Ram Kotaru <123920680+raghuramkotaru@users.noreply.github.com>
2026-06-27 19:15:57 -0400

    Time: 0 ms (100.00%) | Memory: 19.2 MB (55.84%) - LeetSync

---
+++ 66-plus-one/plus-one.py
@@ -0,0 +1,54 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+
+        n = len(digits)-1
+        if digits[n] != 9:
+            digits[n] += 1
+        else:
+            i = n
+            while i>=0 and digits[i]== 9:
+                digits[i] = 0
+                i -= 1
+            if i==-1:
+                digits[0] =1
+                digits.append(0)
+            else:
+                digits[i]+=1
+        return digits
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # n = len(digits)
+        # if digits[n-1] != 9:
+        #     digits[n-1] += 1
+        # else:
+        #     i = n-1
+            
+        #     while i >0 and digits[i] == 9:
+        #         digits[i] = 0
+        #         i -= 1
+        #     digits[i] +=1
+        # return digits
+
+        
