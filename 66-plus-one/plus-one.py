422b40f1e91b8956828998bd4bfe9f2878c67c5e
Raghu Ram Kotaru <123920680+raghuramkotaru@users.noreply.github.com>
2026-06-27 19:17:33 -0400

    Time: 0 ms (100.00%) | Memory: 19.2 MB (87.60%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -10,8 +10,7 @@ class Solution:
                 digits[i] = 0
                 i -= 1
             if i==-1:
-                digits[0] =1
-                digits.append(0)
+                digits = [1]+digits
             else:
                 digits[i]+=1
         return digits
