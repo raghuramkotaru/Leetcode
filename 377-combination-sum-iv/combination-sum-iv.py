098f9848bc76f3aeb28f377264f6556761203219
Raghu Ram Kotaru <123920680+raghuramkotaru@users.noreply.github.com>
2026-07-18 21:04:48 -0400

    Time: 49 ms (63.31%) | Memory: 19.4 MB (31.15%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.py
+++ 377-combination-sum-iv/combination-sum-iv.py
@@ -1,11 +1,58 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        dp = [0] * (target + 1)
-        dp[0] = 1
+        count = 0
+        memo = {}
+        def dfs(n):
+            nonlocal count
+            if n in memo:
+                return memo[n]
+            
+            if n == target:
+                return 1
+            if n > target:
+                return 0
+            ans = 0
+            for i in nums:
+                
+                ans += dfs(n+i)
+            memo[n] = ans
+            return ans
+            
+        return dfs(0)
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # dp = [0] * (target + 1)
+        # dp[0] = 1
         
-        for i in range(1, target + 1):
-            for num in nums:
-                if i - num >= 0:
-                    dp[i] += dp[i - num]
+        # for i in range(1, target + 1):
+        #     for num in nums:
+        #         if i - num >= 0:
+        #             dp[i] += dp[i - num]
                     
-        return dp[target]
+        # return dp[target]
