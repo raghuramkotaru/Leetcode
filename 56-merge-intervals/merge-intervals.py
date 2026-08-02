c8758eaa3b524e045d3a7d027714c71f70c8cff2
Raghu Ram Kotaru <123920680+raghuramkotaru@users.noreply.github.com>
2026-08-02 17:51:21 -0400

    Time: 7 ms (71.72%) | Memory: 22.4 MB (71.10%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,6 +1,7 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort()
+        
+        intervals.sort(key=lambda i:i[0])
         merged = [intervals[0]]
         for i in range(1,len(intervals)):
             left = merged[-1][1]
