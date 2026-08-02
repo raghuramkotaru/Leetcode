beabbe8d244a5796d3c0a535063a7a5abd73b2c7
Raghu Ram Kotaru <123920680+raghuramkotaru@users.noreply.github.com>
2026-08-02 17:50:07 -0400

    Time: 7 ms (71.72%) | Memory: 22.4 MB (71.10%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.py
@@ -0,0 +1,19 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort()
+        merged = [intervals[0]]
+        for i in range(1,len(intervals)):
+            left = merged[-1][1]
+            right = intervals[i][0]
+            if left >= right:
+                curr_mur = [merged[-1][0],max(left,intervals[i][1])]
+                merged[-1] = curr_mur
+                continue
+            merged.append(intervals[i])
+        return merged
+            
+
+            
+
+
+            
